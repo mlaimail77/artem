7b21c8332645ab82f8cad19085300396e837d3fc
João Fiadeiro <j.fiadeiro@gmail.com>
2024-11-30 18:44:56 +0700

    Update bot_server.py

--- bot_server.py
+++ bot_server.py
@@ -37,6 +37,7 @@ async def neynar_webhook():
 
 if __name__ == '__main__':
     app.run(
-        debug=True, 
+        debug=True,
+        host='0.0.0.0',
         port=int(os.getenv('PORT', 3000))
         )
